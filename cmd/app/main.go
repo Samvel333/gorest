ee89478dcc88fd3c5fbea8450e209b0bb31e4178
Samvel333 <sazdav601@gmail.com>
2025-03-29 17:39:01 +0400

    started https server without framework

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	var port int = 5000
+	log.Println("Сервер запущен на порту", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), mux))
 }
